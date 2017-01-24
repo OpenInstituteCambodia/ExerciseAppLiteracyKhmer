47f6e8f6fbc0db07bb538422edef6d0f0c5e6229
Heum Sinat <heumsinatgic25@gmail.com>
2017-01-24 15:43:10 +0700

    create new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,23 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {QuestionType1Page} from '../pages/question-type1/question-type1';
+import {QuestionType2Page} from '../pages/question-type2/question-type2';
+import {QuestionType3Page} from '../pages/question-type3/question-type3';
+import {QuestionType4Page} from '../pages/question-type4/question-type4';
+ import {CongratulationPage} from '../pages/congratulation/congratulation';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    QuestionType1Page,
+    QuestionType2Page,
+    QuestionType3Page,
+    QuestionType4Page,
+     CongratulationPage
+
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +26,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    QuestionType1Page,
+    QuestionType2Page,
+    QuestionType3Page,
+    QuestionType4Page,
+    CongratulationPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
