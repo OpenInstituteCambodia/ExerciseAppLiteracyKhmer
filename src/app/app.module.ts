10418365961e510dcc40ef512c9a3e92bbf4418f
Socheat Sok <socheatsok78@gmail.com>
2017-01-26 10:52:44 +0700

    add media file and media plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MediaPlugin } from 'ionic-native';
+
+// Pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {QuestionType1Page} from '../pages/question-type1/question-type1';
 import {QuestionType2Page} from '../pages/question-type2/question-type2';
 import {QuestionType3Page} from '../pages/question-type3/question-type3';
 import {QuestionType4Page} from '../pages/question-type4/question-type4';
- import {CongratulationPage} from '../pages/congratulation/congratulation';
+import {CongratulationPage} from '../pages/congratulation/congratulation';
 
 
 @NgModule({
