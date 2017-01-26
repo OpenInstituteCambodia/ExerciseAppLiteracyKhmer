1570ece836c16b536c89916ccc032c8610e93ff7
Socheat Sok <socheatsok78@gmail.com>
2017-01-26 14:46:15 +0700

    update btn action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,22 +18,22 @@ export class MyApp {
       StatusBar.styleDefault();
       Splashscreen.hide();
 
-      // Create a MediaPlugin instance.  Expects path to file or url as argument
-      // We can optionally pass a second argument to track the status of the media
-
-      const onStatusUpdate = (status) => console.log(status);
-
-      const file = new MediaPlugin('../assets/audio/1.mp3', onStatusUpdate);
-
-      // Catch the Success & Error Output
-      // Platform Quirks
-      // iOS calls success on completion of playback only
-      // Android calls success on completion of playback AND on release()
-      file.init.then(() => {
-        console.log('Playback Finished');
-      }, (err) => {
-        console.log('somthing went wrong! error code: ' + err.code + ' message: ' + err.message);
-      });
+      // // Create a MediaPlugin instance.  Expects path to file or url as argument
+      // // We can optionally pass a second argument to track the status of the media
+      //
+      // const onStatusUpdate = (status) => console.log(status);
+      //
+      // const file = new MediaPlugin('../assets/audio/1.mp3', onStatusUpdate);
+      //
+      // // Catch the Success & Error Output
+      // // Platform Quirks
+      // // iOS calls success on completion of playback only
+      // // Android calls success on completion of playback AND on release()
+      // file.init.then(() => {
+      //   console.log('Playback Finished');
+      // }, (err) => {
+      //   console.log('somthing went wrong! error code: ' + err.code + ' message: ' + err.message);
+      // });
     });
   }
 }
