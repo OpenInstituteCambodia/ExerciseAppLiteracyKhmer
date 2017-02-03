e1995a15d4f38adab9431b3a68210afcaad18452
Socheat Sok <socheatsok78@gmail.com>
2017-02-03 15:09:56 +0700

    move btn confirm exit application order

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -43,16 +43,19 @@ export class HomePage {
     let alert = this.alert.create({
       title: 'Confirm',
       message: 'Do you want to exit?',
-      buttons: [{
-        text: "Yes",
-        handler: () => {
-          console.log("ivrController: QuestionPage: exit(): Exit Application!")
-          this.platform.exitApp();
+      buttons: [
+        {
+          text: "No",
+          role: 'cancel'
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            console.log("ivrController: QuestionPage: exit(): Exit Application!")
+            this.platform.exitApp();
+          }
         }
-      }, {
-        text: "No",
-        role: 'cancel'
-      }]
+      ]
     })
     alert.present();
   }
--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -64,16 +64,19 @@ export class QuestionPage {
     let alert = this.alert.create({
       title: 'Confirm',
       message: 'Do you want to exit?',
-      buttons: [{
-        text: "Yes",
-        handler: () => {
-          console.log("ivrController: QuestionPage: exit(): Exit Application!")
-          this.platform.exitApp();
+      buttons: [
+        {
+          text: "No",
+          role: 'cancel'
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            console.log("ivrController: QuestionPage: exit(): Exit Application!")
+            this.platform.exitApp();
+          }
         }
-      }, {
-        text: "No",
-        role: 'cancel'
-      }]
+      ]
     })
     alert.present();
   }
