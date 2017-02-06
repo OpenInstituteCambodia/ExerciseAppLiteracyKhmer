d2dca563b727c81e01e4f7ac74e48890219c6905
Socheat Sok <socheatsok78@gmail.com>
2017-02-06 14:50:26 +0700

    add info text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
 
+/*
+  NOTHING TO EDIT HERE !!!
+  GO AWAY!!!
+*/
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp  {
-  public rootPage: any  = HomePage;
+  public rootPage: any  = MenuPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
