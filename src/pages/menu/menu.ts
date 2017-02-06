91419e05b6934f0411f84334b1645ba1225053e7
Socheat Sok <socheatsok78@gmail.com>
2017-02-06 16:24:27 +0700

    MenuPage: button_exit_application(), -> xelaToolbar: exit()

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -3,18 +3,20 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { xelaController } from '../../app/xelaController';
 import { xelaRoute } from '../../app/xelaModule/xelaRoute';
+import { xelaToolbar } from '../../app/xelaModule/xelaToolbar';
 import { QuestionPage } from '../question/question';
 
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
-  providers: [ xelaController, xelaRoute ]
+  providers: [ xelaController, xelaRoute, xelaToolbar ]
 })
 export class MenuPage {
 
   constructor(
     private _xela: xelaController,
-    private _route: xelaRoute) {
+    private _route: xelaRoute,
+    private _toolbar: xelaToolbar) {
 
   }
 
@@ -36,6 +38,7 @@ export class MenuPage {
 
   public button_exit_application(): void {
     console.log("exit app");
+    this._toolbar.exit();
   }
 
 }
