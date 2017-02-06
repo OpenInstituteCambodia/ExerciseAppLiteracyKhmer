a3983fbd294642df956ff34016b200b983d26706
Socheat Sok <socheatsok78@gmail.com>
2017-02-06 10:59:42 +0700

    xelaController: xelaRoute: route(), empty function, use for navigating pages

---
+++ src/app/xelaModule/xelaRoute.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+/*
+  name: "xelaRoute"
+  description: "Self Audio Introduction System, Used for children, literacy or disabled people to help navigate on using the application."
+  author: ""
+  developers: [
+    "Socheat Sok (socheatsok78@gmail.com)",
+    "Sinat Heum (heumsinatgic25@gmail.com)",
+    "Sorya Phoeun (soryaphoeun08@gmail.com)"
+  ]
+  licenses: "GNU GPL"
+*/
+
+@Injectable()
+export class xelaRoute {
+  constructor() {
+
+  }
+
+  public route(page: any): any {
+
+  }
+}
