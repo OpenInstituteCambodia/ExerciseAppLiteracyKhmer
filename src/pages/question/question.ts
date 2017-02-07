5e39ad6eb7f507bc6d23671f10865a889a453d51
Socheat Sok <socheatsok78@gmail.com>
2017-02-07 17:41:46 +0700

    change page route to fit documentation as L{}P{}Q{} of IVR format

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -13,7 +13,12 @@ import { ResultPage } from '../result/result';
   providers: [ xelaController, xelaRoute, xelaToolbar ]
 })
 export class QuestionPage {
+  q_type: any;
+  l_id: any;
+  p_id: any;
   q_id: any;
+
+  q_id_long: any;
 
   // Question Type Filename
   private _M1: string = 'M1.mp3';
@@ -29,14 +34,20 @@ export class QuestionPage {
     private _toolbar: xelaToolbar,
     private _param: NavParams
   ) {
+    this.q_type = this._param.get("q_type");
+    this.l_id = this._param.get("l_id");
+    this.p_id = this._param.get("p_id");
     this.q_id = this._param.get("q_id");
-    if(this.q_id == 9) {
-      this._route.popToRoot();
-    }
+
+    this.q_id_long = "L"+this.l_id+"P"+this.p_id+"Q"+this.q_id;
+    console.log("QuestionPage: this.q_id_long -> ", this.q_id_long);
+    // if(this.q_id == 9) {
+    //   this._route.popToRoot();
+    // }
   }
 
   ionViewDidEnter() {
-    this._xela.play_audio("M"+this.q_id+'.mp3');
+    this._xela.play_audio("M"+this.q_type+'.mp3');
   }
 
   ionViewDidLoad() {
