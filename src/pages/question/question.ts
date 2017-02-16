a1936be55543bb814dac31f59dbd01b0cc23b42b
Socheat Sok <socheatsok78@gmail.com>
2017-02-16 11:15:51 +0700

    Fix Bug when finished question it not closing the page

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -475,6 +475,7 @@ export class QuestionPage {
   public question_next(_id) {
     console.log("this._question_length", this._question_length);
     if (_id > this._question_length) {
+      this.isEnableAnswer = true;
       this.popToRoot();
       return true;
     }
